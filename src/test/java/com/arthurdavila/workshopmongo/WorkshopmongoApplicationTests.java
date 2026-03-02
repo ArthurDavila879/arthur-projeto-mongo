9ac0c15ddb8f2ec1725e9b1d215fb8c22148ef6e
Arthur D'Avila <arthurdavila879@gmail.com>
2026-03-01 21:36:10 -0300

    Projeto iniciado

---
+++ src/test/java/com/arthurdavila/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthurdavila.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
